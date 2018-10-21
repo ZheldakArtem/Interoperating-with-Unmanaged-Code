cedbd97a1967fca3d4de10cc3e0be773dd414f41
Artsem Zhaldak <Artsem_Zhaldak@epam.com>
2018-10-21 20:38:37 +0300

    create tests

---
+++ InteroperatingWithUnmanagedCode/InteroperatingWithUnmanagedCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InteroperatingWithUnmanagedCode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("InteroperatingWithUnmanagedCode")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("777fe04f-41ae-4ba6-96d3-8eb8fd85de96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
