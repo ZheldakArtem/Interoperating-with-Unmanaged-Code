2557f006584b458feedf4adcc09994f08b9ebd62
Artsem Zhaldak <Artsem_Zhaldak@epam.com>
2018-10-22 13:47:03 +0300

    Add LastSleepTime, LastWakeTime

---
+++ InteroperatingWithUnmanagedCode/InteroperatingWithUnmanagedCode/Tasks.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InteroperatingWithUnmanagedCode
+{
+	[TestClass]
+	public class Tasks
+	{
+		const int ProcessorInformation = 11;
+
+		[DllImport("powrprof.dll")]
+		private static extern UInt32 CallNtPowerInformation(
+					Int32 InformationLevel,
+					IntPtr lpInputBuffer,
+					UInt32 nInputBufferSize,
+					out ulong lpOutputBuffer,
+					UInt32 nOutputBufferSize
+					);
+
+
+		// Last last wake time
+		[TestMethod]
+		public void TestLastWakeTime()
+		{
+			ulong lpOutputBuffer = 0;
+
+			uint retval = CallNtPowerInformation(
+					(int)POWER_INFORMATION_LEVEL.LastWakeTime,
+					IntPtr.Zero,
+					0,
+					out lpOutputBuffer,
+					256
+				);
+
+			var status = (NET_API_STATUS)Enum.Parse(typeof(NET_API_STATUS), retval.ToString());
+
+			if (status == NET_API_STATUS.NERR_Success)
+			{
+				Console.WriteLine("Success");
+				Console.WriteLine($"Last Wake Time { lpOutputBuffer / 10000000 / 60} min");
+			}
+
+		}
+
+		// Last last sleep time
+		[TestMethod]
+		public void TestLastSleepTime()
+		{
+			ulong lpOutputBuffer = 0;
+
+			uint retval = CallNtPowerInformation(
+					(int)POWER_INFORMATION_LEVEL.LastSleepTime,
+					IntPtr.Zero,
+					0,
+					out lpOutputBuffer,
+					256
+				);
+
+			var status = (NET_API_STATUS)Enum.Parse(typeof(NET_API_STATUS), retval.ToString());
+
+			if (status == NET_API_STATUS.NERR_Success)
+			{
+				Console.WriteLine("Success");
+				Console.WriteLine($" Last Sleep Time { lpOutputBuffer / 10000000 / 60} min");
+			}
+
+		}
+	}
+}
---
+++ InteroperatingWithUnmanagedCode/PowerManagmentAPI/PowerManagmentAPI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerManagmentAPI
+{
+    public class PowerManagmentAPI
+    {
+		// CallNtPowerInformation
+
+		// 1. Last sleep time
+
+		// 2. Last wake time
+
+		// 3. System battery state
+
+		// 4. System power information
+
+	}
+
+}
+
